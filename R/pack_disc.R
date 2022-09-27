45a1d6890539566f743b882804232ccdfb4070ea
Leonard Norblad <norblad88@gmail.com>
2022-09-27 16:05:26 +0200

    m

---
+++ R/pack_disc.R
@@ -0,0 +1,10 @@
+#' Shiny app for visualizing weather data forecasts.
+#' 
+#' An interactive plot generator. 
+#' 
+#' @docType package
+#' 
+#' @author Leonard Persson Norblad \email{leope744@student.liu.se} Jacob Sibanda \email{jacsi259@student.liu.se}
+#' 
+#' @name ShinyWeatherApp
+NULL
